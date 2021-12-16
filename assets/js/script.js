9de7038a4c8ecfdb1f0924d8d0ee1e112d85513e
nfepb <nicolas.planteb@gmail.com>
2021-12-16 17:22:50 +0000

    Added constant for choices and scoring incrementation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,33 @@
+/** Constants */
+
+const signChoices = [
+    "ROCK",
+    "PAPER",
+    "SCISSORS",
+    "LIZARD",
+    "SPOCK"
+]
+
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-sign") === "select") {
-                checkAnswer();
-            } else {
-                let compareSignChoice = this.getAttribute("data-sign");
-                runGame(compareSignChoice);
-            }
-        });
-    }
+    const selectButton = document.querySelector(".select-btn")
+    selectButton.addEventListener("click", function(){
+        getComputerSignChoice();
+        compareSignChoice();
+        incrementPlayerScore();
+        incrementComputerScore();
+    });
 });
 
 /** Function defining randomly the computer sign */
 
-function computerSignChoice() {
-    return Math.floor((Math.random() * 5) + 1);
+function getComputerSignChoice () {
+    const randomIndex = Math.floor(Math.random() * 5);
+    return signChoices[randomIndex];
 }
 
 /** function comparing player choice and computer choice */
@@ -31,9 +39,13 @@ function calculateWinner () {
 }
 
 function incrementPlayerScore () {
+    let playerScore = parseInt(document.getQuery("player-score").innerText);
+    document.getElementById("player-score").innerText = ++playerScore;
 }
 
 function incrementComputerScore () {
+    let computerScore = parseInt(document.getQuery("computer-score").innerText);
+    document.getElementById("computer-score").innerText = ++computerScore;
 }
 
 /** Constant for the signs */
